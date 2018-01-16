a295c8c57c0740374442518e9e50c8abecc7a6c7
Daniel Almeida <daniel@coletiv.com>
2018-01-16 10:27:55 +0000

    Added nib identifier, improved test cases

---
+++ Example/Tests/Extensions/UIKit/UITextFieldTestCase.swift
@@ -0,0 +1,28 @@
+//
+//  UITextFieldTestCase.swift
+//  Coletiv_Tests
+//
+//  Created by Daniel Almeida on 12/01/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class UITextFieldTestCase: XCTestCase {
+    
+  func testPlaceHolderColor() {
+    
+    let textField: UITextField = UITextField()
+    
+    let placeHolderText = "placeholder_text"
+    
+    textField.placeholder = placeHolderText
+    textField.placeHolderColor = UIColor.green
+    
+    assert(textField.placeHolderColor == UIColor.green)
+    
+    let attributes = [NSAttributedStringKey.foregroundColor: UIColor.green]
+    assert((textField.attributedPlaceholder?.isEqual(to: NSAttributedString(string: placeHolderText, attributes: attributes)))! == true)
+  }
+    
+}
