838c7a208be723f939c50b1492a6289a1d34cb85
Andre Silva <andre.ee11304@gmail.com>
2018-01-17 14:52:00 +0000

    Fixed comments.

--- Coletiv/Source/Extensions/Foundation/Double+Utility.swift
+++ Coletiv/Source/Extensions/Foundation/Double+Utility.swift
@@ -9,9 +9,9 @@ import UIKit
 
 extension Double {
   
-  /// This string contains the float value. If the decimal part = 0 it will not be returned.
-  /// Examples: 1,75 -> "1,75"
-  ///           1,0 -> "1"
+  /// This string contains the double value. If the decimal part = 0 it will not be returned.
+  /// Examples: 1.75 -> "1.75"
+  ///           1.0 -> "1"
   public var clean: String {
     return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
   }
--- Coletiv/Source/Extensions/Foundation/Float+Utility.swift
+++ Coletiv/Source/Extensions/Foundation/Float+Utility.swift
@@ -12,8 +12,8 @@ public extension Float {
   
   
   /// This string contains the float value. If the decimal part = 0 it will not be returned.
-  /// Examples: 1,75 -> "1,75"
-  ///           1,0 -> "1"
+  /// Examples: 1.75 -> "1.75"
+  ///           1.0 -> "1"
   public var clean: String {
     return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
   }
