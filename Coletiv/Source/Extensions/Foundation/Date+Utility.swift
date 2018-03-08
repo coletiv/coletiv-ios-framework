14506ae110b5dd31dfcc15ba9def439c4f3839ab
Daniel Almeida <daniel@coletiv.com>
2018-03-08 16:41:13 +0000

    #170 - interval dates

--- Coletiv/Source/Extensions/Foundation/Date+Utility.swift
+++ Coletiv/Source/Extensions/Foundation/Date+Utility.swift
@@ -19,4 +19,22 @@ public extension Date {
                                            from: date,
                                            to: self).minute ?? 0
   }
+  
+  
+  /// Calculates the interval between 2 dates
+  /// Thanks to: https://stackoverflow.com/questions/40075850/swift-3-find-number-of-calendar-days-between-two-dates/40075983
+  /// - Parameters:
+  ///   - comp: Calendar component to calculate the difference
+  ///   - date: date from the interval will be calculated
+  /// - Returns: Difference in Calendar.Component units
+  public func interval(ofComponent comp: Calendar.Component, fromDate date: Date) -> Int {
+    
+    let currentCalendar = Calendar.current
+    
+    guard let start = currentCalendar.ordinality(of: comp, in: .era, for: date) else { return 0 }
+    guard let end = currentCalendar.ordinality(of: comp, in: .era, for: self) else { return 0 }
+    
+    return end - start
+  }
 }
+
