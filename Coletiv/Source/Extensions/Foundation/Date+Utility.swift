befcb2678e9bfb98fdac6d146576da4db12a582b
Daniel Almeida <daniel@coletiv.com>
2018-03-08 17:24:33 +0000

    #170 - fixed guard

--- Coletiv/Source/Extensions/Foundation/Date+Utility.swift
+++ Coletiv/Source/Extensions/Foundation/Date+Utility.swift
@@ -31,8 +31,10 @@ public extension Date {
     
     let currentCalendar = Calendar.current
     
-    guard let start = currentCalendar.ordinality(of: comp, in: .era, for: date) else { return 0 }
-    guard let end = currentCalendar.ordinality(of: comp, in: .era, for: self) else { return 0 }
+    guard
+      let start = currentCalendar.ordinality(of: comp, in: .era, for: date),
+      let end = currentCalendar.ordinality(of: comp, in: .era, for: self)
+      else { return 0 }
     
     return end - start
   }
